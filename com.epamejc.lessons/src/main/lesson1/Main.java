ed863b7073d547525894d6ce8492010028d7379e
NikolaevArtem <JqXRND7XUVgDJqc>
2019-12-17 17:02:58 +0300

    Minor changes in syntax

--- com.epamejc.lessons/src/main/lesson1/Main.java
+++ com.epamejc.lessons/src/main/lesson1/Main.java
@@ -17,7 +17,7 @@ public class Main {
     }
 
     private static String checkTrafficLight(int n) {
-        String RESET = "\u001B[0m";
+        final String RESET = "\u001B[0m";
         final String RED_BOLD_BRIGHT = "\033[1;91m";
         final String GREEN_BOLD_BRIGHT = "\033[1;92m";
         final String YELLOW_BOLD_BRIGHT = "\033[1;93m";
@@ -28,7 +28,7 @@ public class Main {
         if (0 <= n && n <= 3) result = GREEN_BOLD_BRIGHT + "Зеленый. Проходите!" + RESET;
         else if (3 < n && n <= 5) result = YELLOW_BOLD_BRIGHT + "Желтый. Завершайте движение." + RESET;
         else if (5 < n) result = RED_BOLD_BRIGHT + "Красный. Стойте!" + RESET;
-        else result = "Светофор еще не начал работать. Введите позитивное число. ";
+        else result = "Светофор еще не начал работать. Введите позитивное число";
 
         return result;
     }
