9cd65cbf61d5db2e551b4637c710717581089643
Yury_K <youkofan@gmail.com>
2019-12-17 12:11:53 +0300

    Тест

--- com.epamejc.lessons/src/main/lesson1/Main.java
+++ com.epamejc.lessons/src/main/lesson1/Main.java
@@ -3,6 +3,7 @@ package main.lesson1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("........");
     }
 }
