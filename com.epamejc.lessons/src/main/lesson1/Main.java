96dfed17d0796bc7e2ab717ed3232dd136d735e3
savkindm <savkin200@gmail.com>
2019-12-18 17:58:59 +0300

    first homework

--- com.epamejc.lessons/src/main/lesson1/Main.java
+++ com.epamejc.lessons/src/main/lesson1/Main.java
@@ -1,8 +1,25 @@
 package main.lesson1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        while (true){
+            System.out.print("Input a number: ");
+            int num = in.nextInt();
+            if(num>0 && num<=3){
+                System.out.println("green");
+            }else if (num>3 && num<=5){
+                System.out.println("yellow");
+            }else if(num>5 && num<=10){
+                System.out.println("red");
+            }else {
+                System.out.println(" smth else");
+            }
+        }
     }
+
 }
