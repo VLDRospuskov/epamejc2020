7f985d3bebf0572348b928365deb891d18708a53
AndrewNuzha <andrey.nuzha@gmail.com>
2019-12-18 14:46:58 +0300

    Added solution

---
+++ com.epamejc.lessons/src/main/lesson1/TrafficLight.java
@@ -0,0 +1,41 @@
+package main.lesson1;
+
+import java.util.Scanner;
+
+public class TrafficLight
+{
+    public static void main(String[] args)
+    {
+        String SystemMessage = "Введите количество секунд. Для завершения работы введите пустую строку: ";
+        Scanner scan = new Scanner(System.in);
+        System.out.print(SystemMessage);
+        String inputString = scan.nextLine();
+
+        while (!inputString.isEmpty()) {
+            try {
+                int secQuantity = Integer.parseInt(inputString);
+                String response = DefineTrafficLightSignal(secQuantity);
+                System.out.println(response);
+                System.out.print(SystemMessage);
+                inputString = scan.nextLine();
+            } catch (Exception exc) {
+                System.out.println("Похоже, вы сломали светофор: " + exc.toString());
+                break;
+            }
+        }
+        System.out.println("Программа завершается");
+    }
+
+    private static String DefineTrafficLightSignal(int num)
+    {
+        String result;
+        num = num % 600; //контур на 10 минут
+
+        if (num >= 0 && num <= 180) result = "Зеленый сигнал";
+        else if (num > 180 && num <= 300) result = "Желтый сигнал";
+        else if (num > 300) result = "Красный сигнал";
+        else result = "Светофор еще не включился";
+
+        return result;
+    }
+}
