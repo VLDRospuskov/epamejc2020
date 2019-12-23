c0cc5b6ec1f30d9c771941da83cb5bc9d50ebad2
Arina-s <Arina-s@users.noreply.github.com>
2019-12-23 15:57:09 +0300

    Create Traffic Lights
    Optioin with Scanner

---
+++ com.epamejc.lessons/src/main/homeworks/homework1/TrafficLights.java
@@ -0,0 +1,33 @@
+package com.epam.homeworks.homework1;
+import java.util.Scanner;
+
+public class TrafficLights {
+    static String colorLight;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String firstLine = "Traffic lights on. To turn off the traffic light, enter \"off\". Enter the number of minutes to switch the color: ",
+                lastLine = "You turned off the traffic lights.";
+        System.out.print(firstLine);
+        String input = in.nextLine();
+        while (!input.equals("off")) {
+            try {
+                changeLight(Integer.parseInt(input));
+            } catch (Exception e) {
+                System.out.println("Opps... We don't know this command.");
+            }
+
+            input = in.nextLine();
+        }
+        System.out.print(lastLine);
+    }
+
+    private static void changeLight (int min) {
+        if (min >= 0 && min <= 3)
+            colorLight="Green";
+        if (min >= 4 && min <= 5)
+            colorLight="Yellow";
+        if (min >= 6 && min <= 10)
+            colorLight="Red";
+        System.out.println(colorLight);
+    }
+}
