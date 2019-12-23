cdb444aa57865384cd329b1549e974c74d7671bd
Vladislav Rospuskov <Vladislav_Rospuskov@epam.com>
2019-12-23 20:16:44 +0300

    Add lesson3

--- com.epamejc.lessons/src/main/lessons/lesson3/string/equals_and_hashcode/Main.java
+++ com.epamejc.lessons/src/main/lessons/lesson3/string/equals_and_hashcode/Main.java
@@ -13,6 +13,7 @@ public class Main {
 //        System.out.println(cat1 == cat2);
 //        System.out.println(cat1.equals(cat2));
 
+        String str;
         Set<Cat> hashSet = new HashSet<>();
         System.out.println(cat1.hashCode());
         System.out.println(cat2.hashCode());
