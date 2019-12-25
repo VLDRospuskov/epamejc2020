f6e38643ed8a5b89c61e11f588ab79747242bbc2
Arina-s <Arina-s@users.noreply.github.com>
2019-12-25 16:56:31 +0300

    Add immutable class

---
+++ com.epamejc.lessons/src/main/homeworks/homework2/Main.java
@@ -0,0 +1,12 @@
+package homework2;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        ImmutableClassPerson person = new ImmutableClassPerson("Arina", 21, 1.75, new Date());
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+        System.out.println(person.getHeight());
+        System.out.println(person.getDateOfBirth());
+    }
+}
