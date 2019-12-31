eb0068a1a8dbc7b2e9cc0eab86ae18d21c3db093
Yury_Kofanov <youkofan@gmail.com>
2019-12-31 21:38:34 +0300

    Revert "Тест"
    This reverts commit 9cd65cbf

--- com.epamejc.lessons/src/main/lesson1/Main.java
+++ com.epamejc.lessons/src/main/lesson1/Main.java
@@ -3,7 +3,6 @@ package main.lesson1;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("........");
+        System.out.println("Hello world!");
     }
 }
