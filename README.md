809a8257561cb4309fc070690ae5d6eb922b5e94
Karina <karinaria@yandex.ru>
2020-01-01 16:52:16 +0300

    HW1 - enums have been added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # epamejc2020 \m/
+HW1 - Traffic light
+HW2 - Immutable class
--- com.epamejc.lessons/src/homeworks/hw1/TrafficLight.java
+++ com.epamejc.lessons/src/homeworks/hw1/TrafficLight.java
@@ -4,14 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class TrafficLight {
+import static homeworks.hw1.enums.Messages.*;
 
-    private final static String exitCommand = "exit";
-    private final static String enterMessage = "Enter time in minutes. You can enter not only integer numbers. To exit print \"exit\"";
-    private final static String enterNumberCommand = "You have to enter a number";
-    private final static String greenLightMessage = "Green light is ON";
-    private final static String yellowLightMessage = "Yellow light is ON";
-    private final static String redLightMessage = "Red light is ON";
+public class TrafficLight {
 
     public static void main(String[] args) throws IOException {
 
@@ -21,29 +16,39 @@ public class TrafficLight {
 
         while (cond) {
 
-            System.out.println(enterMessage);
+            System.out.println(ENTER_MESSAGE.toString());
 
             String line = buffer.readLine();
 
-            if (line.equalsIgnoreCase(exitCommand)) {
+            if (line.equalsIgnoreCase(EXIT_COMMAND.toString())) {
                 cond = false;
                 continue;
             }
 
             Double minutes = getNumberFromInput(line);
-            if (minutes != null) getColor(minutes);
+            if (minutes != null) {
+                getColor(minutes);
+            }
 
         }
+
+        buffer.close();
     }
 
     private static Double getNumberFromInput(String line) {
 
+        Double d = null;
+
         try {
-            return Double.parseDouble(line);
+            if (Double.parseDouble(line) > 0) {
+                return Double.parseDouble(line);
+            } else {
+                System.out.println(ENTER_NUMBER_COMMAND.toString());
+            }
         } catch (NumberFormatException e) {
-            System.out.println(enterNumberCommand);
+            System.out.println(ENTER_NUMBER_COMMAND.toString());
         }
-        return null;
+        return d;
     }
 
     private static void getColor(double minutes) {
@@ -51,11 +56,11 @@ public class TrafficLight {
         double div = minutes % 10;
 
         if (div >= 0 && div < 3) {
-            System.out.println(greenLightMessage);
+            System.out.println(GREEN_LIGHT_MESSAGE.toString());
         } else if (div >= 3 && div < 5) {
-            System.out.println(yellowLightMessage);
+            System.out.println(YELLOW_LIGHT_MESSAGE.toString());
         } else {
-            System.out.println(redLightMessage);
+            System.out.println(RED_LIGHT_MESSAGE.toString());
         }
     }
 }
