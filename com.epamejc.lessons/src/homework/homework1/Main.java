1ebdd28e420adb5dee92e1047734a1ef0c17c754
shandakova <shandakova98@mail.ru>
2020-01-03 19:38:24 +0300

    change homework package

--- com.epamejc.lessons/src/homework/homework1/Main.java
+++ com.epamejc.lessons/src/homework/homework1/Main.java
@@ -1,4 +1,4 @@
-package homework1;
+package homework.homework1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- com.epamejc.lessons/src/homework/homework1/TrafficLight.java
+++ com.epamejc.lessons/src/homework/homework1/TrafficLight.java
@@ -1,4 +1,4 @@
-package homework1;
+package homework.homework1;
 public class TrafficLight {
     private enum Color{
         GREEN,
