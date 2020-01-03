887be968bff6e000c81cea11cd1456ae8ff278dc
Alex F <desdichago@gmail.com>
2020-01-03 21:39:53 +0300

    TL wip

--- com.epamejc.lessons/src/main/lesson1/Main.java
+++ com.epamejc.lessons/src/main/lesson1/Main.java
@@ -1,8 +1,10 @@
 package main.lesson1;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TrafficLight trafficLight = new TrafficLight();
+        trafficLight.timer(1);
+        trafficLight.timer("1");
+        trafficLight.timer("1:10");
     }
 }
---
+++ com.epamejc.lessons/src/main/lesson1/TrafficLight.java
@@ -0,0 +1,18 @@
+package main.lesson1;
+
+import java.util.Scanner;
+
+public class TrafficLight {
+    private int minutes;
+    private int seconds = 0;
+
+    public String timer(int time) {
+
+        return "";
+    }
+
+    public String timer(String timeString) {
+
+        return "";
+    }
+}
