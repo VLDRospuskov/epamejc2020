5c9e42ace635fc1af88e84ba2b1af75da24a7742
Alex F <desdichago@gmail.com>
2020-01-07 16:30:10 +0300

    TL wip

--- com.epamejc.lessons/src/main/lesson1/Main.java
+++ com.epamejc.lessons/src/main/lesson1/Main.java
@@ -3,8 +3,6 @@ package main.lesson1;
 public class Main {
     public static void main(String[] args) {
         TrafficLight trafficLight = new TrafficLight();
-        trafficLight.timer(1);
-        trafficLight.timer("1");
-        trafficLight.timer("1:10");
+        trafficLight.start();
     }
 }
--- com.epamejc.lessons/src/main/lesson1/TrafficLight.java
+++ com.epamejc.lessons/src/main/lesson1/TrafficLight.java
@@ -1,18 +1,81 @@
 package main.lesson1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class TrafficLight {
     private int minutes;
-    private int seconds = 0;
+    private int seconds;
+    private final String welcomeMessage = "Welcome to TrafficLight! \nPlease input time to check. \nUse MM or MM:SS format. \nType q to exit program.";
 
-    public String timer(int time) {
+    private InputStreamReader inputStreamReader;
+    private BufferedReader bufferedReader;
+    private boolean streamIsActive = false;
 
-        return "";
+    private String inputString;
+
+    public void start() {
+        System.out.println(welcomeMessage);
+        startStream();
+        readInput();
     }
 
-    public String timer(String timeString) {
+    private void readInput() {
+        try {
+            while ((inputString = bufferedReader.readLine()) != null) {
+                System.out.println(inputString);
+                /* exit */
+                if (inputString.equals("q")) {
+                    stopStream();
+                    break;
+                }
+                if (isInteger(inputString)) {
 
-        return "";
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startStream() {
+        inputStreamReader = new InputStreamReader(System.in);
+        bufferedReader = new BufferedReader(inputStreamReader);
+        streamIsActive = true;
+    }
+
+    private void stopStream() {
+        try {
+            inputStreamReader.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        streamIsActive = false;
+    }
+
+    private boolean isInteger(String str) {
+        if (str == null) {
+            return false;
+        }
+        int length = str.length();
+        if (length == 0) {
+            return false;
+        }
+        int i = 0;
+        if (str.charAt(0) == '-') {
+            if (length == 1) {
+                return false;
+            }
+            i = 1;
+        }
+        for (; i < length; i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
     }
 }
