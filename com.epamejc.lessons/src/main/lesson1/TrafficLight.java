f0b0e3ec58860f9102af4e819e7c63a65a571426
Alex F <desdichago@gmail.com>
2020-01-08 20:32:32 +0300

    TrafficLight with one number input

--- com.epamejc.lessons/src/main/lesson1/TrafficLight.java
+++ com.epamejc.lessons/src/main/lesson1/TrafficLight.java
@@ -6,8 +6,10 @@ import java.io.InputStreamReader;
 
 public class TrafficLight {
     private int minutes;
-    private int seconds;
-    private final String welcomeMessage = "Welcome to TrafficLight! \nPlease input time to check. \nUse MM or MM:SS format. \nType q to exit program.";
+    private boolean outputIsReady = false;
+    private final String welcomeMessage = "Welcome to TrafficLight! \nPlease input time in minutes to check. \nType q to exit program.";
+    private final String goodbyeMessage = "Thank you for using TrafficLight, goodbye!";
+    private final String stringMismatchMessage = "Please provide time in minutes.";
 
     private InputStreamReader inputStreamReader;
     private BufferedReader bufferedReader;
@@ -19,19 +21,34 @@ public class TrafficLight {
         System.out.println(welcomeMessage);
         startStream();
         readInput();
+        System.out.println(goodbyeMessage);
     }
 
     private void readInput() {
         try {
             while ((inputString = bufferedReader.readLine()) != null) {
+                outputIsReady = false;
                 System.out.println(inputString);
                 /* exit */
                 if (inputString.equals("q")) {
                     stopStream();
                     break;
                 }
-                if (isInteger(inputString)) {
 
+                /* input is digit(s) */
+                if (isInteger(inputString)) {
+                    minutes = Integer.parseInt(inputString);
+                    while (minutes >= 10) {
+                        minutes = minutes - 10;
+                    }
+                    outputIsReady = true;
+                } else {
+                    System.out.println(stringMismatchMessage);
+                }
+
+                if (outputIsReady) {
+                    checkLight();
+                    printOutput();
                 }
             }
         } catch (IOException e) {
@@ -77,5 +94,19 @@ public class TrafficLight {
             }
         }
         return true;
+    }
+
+    private void printOutput() {
+        System.out.println("output: " + minutes);
+    }
+
+    private void checkLight() {
+        if (minutes < 4) {
+            System.out.println("Light is GREEN");
+        } else if (minutes < 6) {
+            System.out.println("Light is YELLOW");
+        } else {
+            System.out.println("Light is RED");
+        }
     }
 }
