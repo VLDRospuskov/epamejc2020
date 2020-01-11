ceb7e843bda6aa14f35b5d0302a9f9610f55035d
Arina-s <Arina-s@users.noreply.github.com>
2020-01-12 01:56:55 +0300

    Refactoring code
    Removed unnecessary methods, added new ones

--- com.epamejc.lessons/src/main/homeworks/homework2/arrays/generetionCharArray/RandomLetterGeneration.java
+++ com.epamejc.lessons/src/main/homeworks/homework2/arrays/generetionCharArray/RandomLetterGeneration.java
@@ -6,63 +6,53 @@ public class RandomLetterGeneration {
     private char[][] array;
     private int n;
     private int m;
+    private String output = "";
 
     public void run() {
-        System.out.print("Please, enter the dimension of the array: ");
-        Scanner in = new Scanner(System.in);
-        try {
-            n = in.nextInt();
-            m = in.nextInt();
-        } catch (Exception e) {
-            System.out.println("You entered invalid value :(");
-        }
-        setN(n);
-        setM(m);
+        inputDimension();
         createArray();
         fillArray();
         outputArray();
     }
 
-    public void setN(int n) {
-        if (n > 0) {
-            this.n = n;
-        } else {
-            System.out.println("You entered the negative dimension or zero!");
-        }
-    }
-
-    public void setM(int m) {
-        if (m > 0) {
-            this.m = m;
-        } else {
-            System.out.println("You entered the negative dimension or zero!");
-        }
-    }
-
-    public void createArray() {
-        if (n != 0 && m != 0) {
-            array = new char[n][m];
-        }
-    }
-
-    public void fillArray() {
-        if (n != 0 && m != 0) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    array[i][j] = (char) (Math.random() * 26 + 97);
+    private void inputDimension() {
+        System.out.print("Please, enter the dimension of the array: ");
+        Scanner inInt = new Scanner(System.in);
+        boolean valueOk = false;
+        while (!valueOk) {
+            try {
+                n = inInt.nextInt();
+                m = inInt.nextInt();
+                if (n <= 0 || m <= 0) {
+                    throw new ArithmeticException();
                 }
+                valueOk = true;
+            } catch (ArithmeticException e) {
+                System.out.println("Enter a positive value.");
+            } catch (Exception e) {
+                System.out.println("You entered the invalid value :(");
+            }
+        }
+    }
+
+    private void createArray() {
+        array = new char[n][m];
+    }
+
+    private void fillArray() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                array[i][j] = (char) (Math.random() * 26 + 97);
             }
         }
     }
 
     public void outputArray() {
-        if (n != 0 && m != 0) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    System.out.print(array[i][j] + " ");
-                }
-                System.out.println();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(array[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
