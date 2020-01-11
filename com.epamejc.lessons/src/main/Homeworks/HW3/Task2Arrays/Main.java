2cb040e661b9bc31a5d35d7ee6002f6f91ffb5fc
Alex F <desdichago@gmail.com>
2020-01-11 23:47:54 +0300

    HW3 Task 2.3

--- com.epamejc.lessons/src/main/Homeworks/HW3/Task2Arrays/Main.java
+++ com.epamejc.lessons/src/main/Homeworks/HW3/Task2Arrays/Main.java
@@ -5,7 +5,10 @@ public class Main {
         //T21TwoDimensional t21 = new T21TwoDimensional();
         //t21.start();
 
-        T22TwoDimensionalWithString t22 = new T22TwoDimensionalWithString();
-        t22.start();
+        //T22TwoDimensionalWithString t22 = new T22TwoDimensionalWithString();
+        //t22.start();
+
+        T23Pyramid t23 = new T23Pyramid();
+        t23.start(10);
     }
 }
