da0ce5ed684d7f551efccb8335fe81f2f90d936b
Alex F <desdichago@gmail.com>
2020-01-11 23:25:04 +0300

    HW3 Task 2.1

---
+++ com.epamejc.lessons/src/main/Homeworks/HW3/Task2Arrays/T211TwoDimensional.java
@@ -0,0 +1,36 @@
+package main.Homeworks.HW3.Task2Arrays;
+
+/*
+2.1. Создать массив массивов, задается размерность на основании которое генерируются случайные символы типа char. Пример:
+Input: 4 2
+output:
+k c
+b p
+b h
+h g
+*/
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class T211TwoDimensional {
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print((char) getRandomInt() + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+
+    private int getRandomInt() {
+        return (int) ((Math.random() * (126 - 33)) + 33);
+    }
+}
