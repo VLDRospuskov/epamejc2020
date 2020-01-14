1de05542c76f38587fc0f30d583d4643a21c9a47
Karpovich Art <karpovichart@gmail.com>
2020-01-14 19:38:59 +0300

    1 homework fixed

--- com.epamejc.lessons/src/main/lesson1/Main.java
+++ com.epamejc.lessons/src/main/lesson1/Main.java
@@ -6,54 +6,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String min;
-        while (true) {
+        boolean exit = true;
+        while (exit) {
             System.out.print("Enter sec (-1 to exit): ");
             min = in.nextLine();
             System.out.println();
-            if (isNum(min)) {
-                if (Integer.parseInt(min) >= 0 && Integer.parseInt(min) < 4) {
-                    System.out.println("Green");
-
-                } else if (Integer.parseInt(min) >= 4 && Integer.parseInt(min) < 6) {
-                    System.out.println("Yellow");
-
-                } else if (Integer.parseInt(min) >= 6 && Integer.parseInt(min) <= 10) {
-                    System.out.println("Red");
-
-                } else if (Integer.parseInt(min) == (-1)) {
-                    return;
-                } else {
-                    System.out.println("Error1");
-
-                }
-            } else {
-                System.out.println("Error2");
+            if (Integer.parseInt(min) == (-1)) {
+                break;
             }
-//            switch (Integer.parseInt(sec)) {
-//                case (-1): {
-//                    return;
-//                }
-//                case 0 - 180: {
-//                    System.out.print("Green");
-//                    break;
-//                }
-//                case (181 - 300): {
-//                    System.out.print("Yellow");
-//                    break;
-//                }
-//                case (301 - 600): {
-//                    System.out.print("Red");
-//                    break;
-//                }
-//                default: {
-//                    System.out.print("Error"+Integer.parseInt(sec));
-//                    break;
-//                }
-//            }
+            System.out.print(selectColor(min));
         }
     }
 
     public static Boolean isNum(String str) {
         return str.matches("[-+]?\\d+");
+    }
+
+    public static String selectColor(String min) {
+
+        if (isNum(min)) {
+            if (Integer.parseInt(min) >= 0 && Integer.parseInt(min) < 4) {
+                return ("Green");
+
+            } else if (Integer.parseInt(min) >= 4 && Integer.parseInt(min) < 6) {
+                return ("Yellow");
+
+            } else if (Integer.parseInt(min) >= 6 && Integer.parseInt(min) <= 10) {
+                return ("Red");
+
+            } else {
+                return ("Error");
+
+            }
+        } else {
+            return ("Error");
+        }
     }
 }
