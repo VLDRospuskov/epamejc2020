d2615be66d25e0f660348a17ce4793cbd3b4a45e
Vitaly Bogdanoff <nightstranger@mail.ru>
2020-01-15 18:26:17 +0300

    Add StringOddEven (Homework2 - Strings 3.1)

---
+++ com.epamejc.lessons/src/homeworks/hw2/strings_3_1/StringOddEven.java
@@ -0,0 +1,35 @@
+package homeworks.hw2.strings_3_1;
+
+import java.util.*;
+
+public class StringOddEven {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = "";
+        String type = "";
+
+        try {
+            str = scan.next();
+            type = scan.next();
+        } catch (Exception e) {
+            System.out.println("Error! " + e);
+        } finally {
+            scan.close();
+        }
+
+        String output = "";
+
+        if (type.equals("even")) {
+            for (int i = 1; i < str.length(); i += 2) {
+                output += str.charAt(i);
+            }
+        } else if (type.equals("odd")) {
+            for (int i = 0; i < str.length(); i += 2) {
+                output += str.charAt(i);
+            }
+        } else {
+            System.out.println("Wrong enter!");
+        }
+        System.out.println(output);
+    }
+}
