9d42366027913d139d02b78a64654a0c6abd2c4b
Vitaly Bogdanoff <nightstranger@mail.ru>
2020-01-15 19:14:11 +0300

    Add StringByIndex (Homework2 - Strings 3.2)

---
+++ com.epamejc.lessons/src/homeworks/hw2/strings_3_2/Readme.txt
@@ -0,0 +1,5 @@
+Написать программу которая будет генерировать строку по индексам. Пример:
+Input: Hello World,
+input: 0  output: H,
+input: 4  output: Ho,
+input: 6 output: HoW
---
+++ com.epamejc.lessons/src/homeworks/hw2/strings_3_2/StringByIndex.java
@@ -0,0 +1,47 @@
+package homeworks.hw2.strings_3_2;
+
+import java.util.Scanner;
+
+public class StringByIndex {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        String str = "";
+
+        System.out.print("Please enter text: ");
+        try {
+            str = scan.nextLine();
+        } catch (Exception e) {
+            System.out.println("Error! " + e);
+        }
+
+        int index = 0;
+        String output = "";
+        boolean loop = true;
+        String msg = "Please enter index between 0 and " + (str.length() - 1) + " (-1 to exit): ";
+
+        while (loop) {
+            Scanner scan2 = new Scanner(System.in);
+            System.out.print(msg);
+            try {
+                index = scan2.nextInt();
+            } catch (Exception e) {
+                System.out.println("Error! " + e);
+            }
+
+            if (index == -1 ) { //
+                break;
+            }
+
+            if (index < 0 || index > str.length() - 1) {
+                System.out.println("Wrong index!");
+                continue;
+            } else {
+                output += str.charAt(index);
+                System.out.println(output);
+            }
+        }
+
+    }
+}
