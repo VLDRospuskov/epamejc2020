628d0d0fe6b43167366c08d5e9d1e41abf40cfd3
Vitaly Bogdanoff <nightstranger@mail.ru>
2020-01-15 13:58:54 +0300

    Refactor TrafficLight after code review. (Homework 1)

--- com.epamejc.lessons/src/homeworks/hw1/TrafficLight.java
+++ com.epamejc.lessons/src/homeworks/hw1/TrafficLight.java
@@ -3,22 +3,9 @@ package homeworks.hw1;
 import java.util.Scanner;
 
 class TrafficLight {
-    public int readNumber() {
-        int n = 0;
-        System.out.print("Enter number(enter \"-1\" to exit): ");
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        try {
-            n = Integer.parseInt(str);
-        } catch (Exception e) {
-            System.out.println("Wrong enter! Please enter number!");
-        } finally {
-            //sc.close();
-        }
-        return n;
-    }
+    String errMessage = "Wrong enter! Please enter positive number!";
 
-    public int checkN (int n){
+    public int checkN(int n) { // приводим n к виду от 0 до 10
         if (n > 10) {
             while (n > 10) {
                 n = n % 10;
@@ -27,24 +14,36 @@ class TrafficLight {
         return n;
     }
 
-    public String outColor (int n) {
+    public String colorOut(int n) {
         if (n >= 0 && n <= 3) {
             return "Green";
         } else if (n >= 4 && n <= 5) {
             return "Yellow";
-        } else {
+        } else if (n >= 6 && n <= 10) {
             return "Red";
         }
+        return errMessage; // ошибка на случай отрицательных чисел
+
     }
 
     public void run() {
+        Scanner sc = new Scanner(System.in);
         boolean loop = true;
         while (loop) {
-            int n = checkN(readNumber());
+            int n = 0;
+            System.out.print("Enter number(enter \"-1\" to exit): ");
+            String str = sc.nextLine();
+            try {
+                n = checkN(Integer.parseInt(str));
+            } catch (Exception e) {
+                System.out.println(errMessage);
+                continue;
+            }
             if (n == -1) {
                 break;
             }
-            System.out.println("Traffic signal: " + outColor(n));
+            System.out.println("Traffic signal: " + colorOut(n));
         }
+        sc.close();
     }
 }
