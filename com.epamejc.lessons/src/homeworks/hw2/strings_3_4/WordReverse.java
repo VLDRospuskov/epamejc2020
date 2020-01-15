60bb5a9d272935597c446c92467cd286d8a0710b
Vitaly Bogdanoff <nightstranger@mail.ru>
2020-01-15 21:08:41 +0300

    Add WordReverse (Homework2 - Strings 3.4)

---
+++ com.epamejc.lessons/src/homeworks/hw2/strings_3_4/WordReverse.java
@@ -0,0 +1,33 @@
+package homeworks.hw2.strings_3_4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordReverse {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = "";
+        System.out.print("Please enter text: ");
+        try {
+            str = scan.nextLine();
+        } catch (Exception e) {
+            System.out.println("Error! " + e);
+        } finally {
+            scan.close();
+        }
+
+        String[] words = str.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            String revStr = "";
+            for (int j = words[i].length() - 1; j >= 0 ; j--) {
+                revStr += words[i].charAt(j);
+            }
+            words[i] = revStr;
+        }
+
+        for (int k = 0; k < words.length; k++) {
+            System.out.print(words[k] + " ");
+        }
+    }
+}
