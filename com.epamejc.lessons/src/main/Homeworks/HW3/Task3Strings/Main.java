316f5c33758a1f2ecf2f4f9c596d49bbea3f6625
Alex F <desdichago@gmail.com>
2020-01-16 17:37:19 +0300

    HW3 Task 3 Strings

---
+++ com.epamejc.lessons/src/main/Homeworks/HW3/Task3Strings/Main.java
@@ -0,0 +1,11 @@
+package main.Homeworks.HW3.Task3Strings;
+
+public class Main {
+    public static void main(String[] args) {
+        //T31EvenOdd t31 = new T31EvenOdd();
+        //t31.start();
+
+        T32Index t32 = new T32Index();
+        t32.start();
+    }
+}
