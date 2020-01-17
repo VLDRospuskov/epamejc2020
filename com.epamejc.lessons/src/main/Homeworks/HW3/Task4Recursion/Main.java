ef06a598f7d59c93661e472cc78522e5d1e82650
Alex F <desdichago@gmail.com>
2020-01-17 11:48:00 +0300

    HW3 Task 4.1

---
+++ com.epamejc.lessons/src/main/Homeworks/HW3/Task4Recursion/Main.java
@@ -0,0 +1,8 @@
+package main.Homeworks.HW3.Task4Recursion;
+
+public class Main {
+    public static void main(String[] args) {
+        T41Fibonacci t41 = new T41Fibonacci();
+        t41.start();
+    }
+}
