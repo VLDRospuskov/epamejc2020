ecd98893d979c619d4dbfb19fdf48b07de5eacb8
Vitaly Bogdanoff <nightstranger@mail.ru>
2020-01-17 19:34:51 +0300

    Play with ImmutableInt (Homework2 - ImmutableInt 1)

---
+++ com.epamejc.lessons/src/homeworks/hw2/immutableClass_1_1/TestImInt.java
@@ -0,0 +1,15 @@
+package homeworks.hw2.immutableClass_1_1;
+
+public class TestImInt {
+    public static void main(String[] args) {
+        ImmutableInt ii = new ImmutableInt(17);
+        ImmutableInt i2 = new ImmutableInt(256);
+
+        System.out.println("System.out.println(ii): " + ii); // link to ii instance
+        // System.out.println(ii.value); // wrong
+        System.out.println("System.out.println(ii.getValue()): " + ii.getValue());
+        // int i = ii.value; // wrong
+        int i = i2.getValue();
+        System.out.println("int i = ii.getValue()\nSystem.out.println(i): " + i);
+    }
+}
