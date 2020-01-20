0db5825828e12ac4ec5fec94f9361cef4653041d
Kirill Turchinskii <turchinskii.kirill@gmail.com>
2020-01-20 14:57:05 +0300

    Task 2.3 is done.

---
+++ com.epamejc.lessons/src/main/lesson3/task2_Arrays/task2_3/Task2_3.java
@@ -0,0 +1,8 @@
+package main.lesson3.task2_Arrays.task2_3;
+
+public class Task2_3 {
+    public static void main(String[] args) {
+        PyramidArray pyramidArray = new PyramidArray();
+        pyramidArray.createPyramidArray();
+    }
+}
