beb78716d20b1c3b45e1e9f72e0d370ac22cf562
Kirill Turchinskii <turchinskii.kirill@gmail.com>
2020-01-20 16:23:02 +0300

    Task 3.2 is done.

---
+++ com.epamejc.lessons/src/main/lesson3/task3_Strings/task3_2/GenString.java
@@ -0,0 +1,31 @@
+package main.lesson3.task3_Strings.task3_2;
+
+import java.util.Scanner;
+
+public class GenString {
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input source string");
+        String sourceString = scanner.nextLine();
+        StringBuffer stringBuffer = new StringBuffer();
+        String inputString;
+        int nextInt = -1;
+        do {
+            System.out.println("Input char number");
+            inputString = scanner.nextLine();
+            try {
+                nextInt = Integer.parseInt(inputString);
+            } catch (NumberFormatException e) {
+                System.out.println("Use only positive numbers");
+                nextInt = -1;
+            }
+            if ((nextInt < sourceString.length()) && (nextInt >= 0)) {
+                stringBuffer.append(sourceString.charAt(nextInt));
+            } else {
+                System.out.println("Number is too big or too low");
+            }
+            System.out.println(stringBuffer.toString());
+        } while (!inputString.isEmpty());
+    }
+}
---
+++ com.epamejc.lessons/src/main/lesson3/task3_Strings/task3_2/Task3_2.java
@@ -0,0 +1,9 @@
+package main.lesson3.task3_Strings.task3_2;
+
+public class Task3_2 {
+
+    public static void main(String[] args) {
+        GenString genString = new GenString();
+        genString.run();
+    }
+}
