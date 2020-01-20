7212d33e600ffc9e3c3d7e89d8219057e3dc02d2
Kirill Turchinskii <turchinskii.kirill@gmail.com>
2020-01-20 15:38:05 +0300

    Task 3.1 is done.

---
+++ com.epamejc.lessons/src/main/lesson3/task3_Strings/task3_1/OddEvenSubstring.java
@@ -0,0 +1,45 @@
+package main.lesson3.task3_Strings.task3_1;
+
+import java.util.Scanner;
+
+public class OddEvenSubstring {
+
+    private String createStringFromOdd(String s) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < s.length(); i += 2) {
+            stringBuffer.append(s.charAt(i));
+        }
+        return stringBuffer.toString();
+    }
+
+    private String createStringFromEven(String s) {
+        StringBuffer stringBuffer = new StringBuffer();
+        if (s.length() > 0) {
+            for (int i = 1; i < s.length(); i += 2) {
+                stringBuffer.append(s.charAt(i));
+            }
+        } else {
+            System.out.println("String is too short for even");
+        }
+
+        return stringBuffer.toString();
+    }
+
+    public String run() {
+        System.out.println("Input string to transform");
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        System.out.println("Input \"odd\" or \"even\"");
+        String mod = scanner.nextLine().toLowerCase();
+        switch (mod) {
+        case "even":
+            return createStringFromEven(inputString);
+        case "odd":
+            return createStringFromOdd(inputString);
+        default:
+            System.out.println("wrong mod input");
+            return "";
+        }
+    }
+
+}
---
+++ com.epamejc.lessons/src/main/lesson3/task3_Strings/task3_1/Task3_1.java
@@ -0,0 +1,9 @@
+package main.lesson3.task3_Strings.task3_1;
+
+public class Task3_1 {
+
+    public static void main(String[] args) {
+        OddEvenSubstring oddEvenSubstring = new OddEvenSubstring();
+        System.out.println(oddEvenSubstring.run());
+    }
+}
