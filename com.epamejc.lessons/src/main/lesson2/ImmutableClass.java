597b2a5f9c469ac31f78801458a40dc4c50f96dc
koshreality <koshreality@gmail.com>
2020-01-23 19:49:01 +0300

    Update ImmutableClass.java

--- com.epamejc.lessons/src/main/lesson2/ImmutableClass.java
+++ com.epamejc.lessons/src/main/lesson2/ImmutableClass.java
@@ -1,23 +1,62 @@
 package main.lesson2;
-/*
-Don't provide "setter" methods
-Make all fields final and private.
-Don't allow subclasses to override methods.
-If the instance fields include references to mutable objects, don't allow those objects to be changed
-*/
+public final class ImmutableClass implements Cloneable {
 
 
-public final class ImmutableClass {
-
-
-    public ImmutableClass(int a) {
-        this.a = a;
+    ImmutableClass(int a) {
+        this.value = a;
     }
 
-    private final int a;
+    private final int value;
 
-    public int getA() {
-        return a;
+    @Override
+    public String toString() {
+        return Integer.toString(this.value);
     }
-    
+
+    int getValue() {
+        return this.value;
+    }
+
+    ImmutableClass copy() {
+        return new ImmutableClass(this.value);
+    }
+
+    ImmutableClass toPlusOne() {
+        return new ImmutableClass(this.value + 1);
+    }
+
+    ImmutableClass plus(ImmutableClass other) {
+        return new ImmutableClass(this.value + other.value);
+    }
+
+    ImmutableClass XOR (ImmutableClass a){
+        return new ImmutableClass(this.value ^ a.getValue());
+    }
+    @Override
+    public boolean equals(Object a){
+        return a instanceof ImmutableClass && ((ImmutableClass) a).value == this.value;
+    }
+
+    public static void main(String[] args) {
+
+        ImmutableClass immutableObject = new ImmutableClass(5);
+        ImmutableClass b = immutableObject.toPlusOne();
+        System.out.println(immutableObject);
+        System.out.println(b);
+
+        ImmutableClass c = immutableObject.plus(b);
+        System.out.println(c);
+
+        int a = b.getValue();
+        System.out.println(a^2);
+        System.out.println(b.XOR(new ImmutableClass(2)));
+
+        try {
+            System.out.println(b.clone().equals(b.copy()));
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+    }
 }
+
