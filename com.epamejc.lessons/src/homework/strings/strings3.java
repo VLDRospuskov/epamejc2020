6b4f3e8011e46001a2a74bba5eb5ea7dbac15cdb
DarPru <darya.prusowa@gmail.ru>
2020-01-24 23:56:46 +0300

    Ex. 3.2.

---
+++ com.epamejc.lessons/src/homework/strings/strings3.java
@@ -0,0 +1,25 @@
+package homework.strings;
+import java.util.Scanner;
+
+public class strings3 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input your string: ");
+        String stringIn = in.nextLine();
+
+        System.out.print("Input the first index, then press 'enter' to input the second one ");
+        int index1 = in.nextInt();
+        int index2 = in.nextInt();
+
+
+        StringBuffer ScrambleString = new StringBuffer(stringIn);
+        char symbol1 = ScrambleString.charAt(index1);
+        char symbol2 = ScrambleString.charAt(index2);
+        char cash = symbol2;
+        ScrambleString.setCharAt(index2, symbol1);
+        ScrambleString.setCharAt(index1, cash);
+        System.out.println(ScrambleString.toString());
+
+    }
+}
