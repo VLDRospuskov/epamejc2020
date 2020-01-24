2fae9e81609854f92b11ed0dfb8daca9a6023ed2
NikolaevArtem <JqXRND7XUVgDJqc>
2020-01-24 20:29:31 +0300

    Refined Fibonacci in recursion homework.

--- com.epamejc.lessons/src/homeworks/newYearHolidays/recursion/fibonacci/Fibonacci.java
+++ com.epamejc.lessons/src/homeworks/newYearHolidays/recursion/fibonacci/Fibonacci.java
@@ -1,19 +1,55 @@
 package homeworks.newYearHolidays.recursion.fibonacci;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Fibonacci {
-    public static void main(String[] args) {
+class Fibonacci {
+    void run() {
+
+        System.out.println("Для выхода из программы введите \"exit\"");
+
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            int n = Integer.parseInt(reader.readLine());
-            System.out.println(fibonacci(n));
-        } catch (Exception e) {
+            String s;
+
+            do {
+                System.out.print("Введите порядковый номер числа Фибоначчи: ");
+
+                s = reader.readLine().trim();
+                int input = parseIntWithExceptions(s);
+                long start = System.currentTimeMillis();
+                int number = fibonacci(input);
+                long finish = System.currentTimeMillis();
+
+                System.out.println("Значение " + input + " числа Фибоначчи: " + number +
+                        ". Найдено за " + (finish-start) + " миллисекунд");
+            } while (!s.equals("exit"));
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static int fibonacci (int n) {
+    private int parseIntWithExceptions (String s) {
+        try {
+            int n = Integer.parseInt(s);
+
+            if (n < 0) {
+                System.out.println("\033[1;91m" + "Введите положительное число!" + "\u001B[0m");
+            }
+
+            return Math.max(n, 0);
+        } catch (Exception e) {
+            System.out.println("\033[1;91m" + "Введено некорректное значение!" + "\u001B[0m");
+            return 0;
+        }
+    }
+
+    private int fibonacci (int n) {
+        if (n > 50) {
+            n = 50;
+        }
+
         if (n == 0) {
             return 0;
         } else if (n == 1) {
---
+++ com.epamejc.lessons/src/homeworks/newYearHolidays/recursion/fibonacci/Main.java
@@ -0,0 +1,7 @@
+package homeworks.newYearHolidays.recursion.fibonacci;
+
+public class Main {
+    public static void main(String[] args) {
+        new Fibonacci().run();
+    }
+}
