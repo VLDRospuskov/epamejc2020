8d767fb20b9ecddcb8fbdaaa9b666cfa40683850
Vitaly Bogdanoff <nightstranger@mail.ru>
2020-01-24 18:49:29 +0300

    Refactor TrafficLight HW1

--- com.epamejc.lessons/src/main/homeworks/hw1/Main.java
+++ com.epamejc.lessons/src/main/homeworks/hw1/Main.java
@@ -1,6 +1,6 @@
 package main.homeworks.hw1;
 
-public class HomeWork1 {
+public class Main {
     public static void main(String[] args) {
         TrafficLight tl = new TrafficLight();
         tl.run();
