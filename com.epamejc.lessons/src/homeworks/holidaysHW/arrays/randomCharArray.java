871dc3ce9e3ac7469783484a9efdffefc583ea98
Neirwin <nightablackstone@gmail.com>
2020-01-25 07:43:25 +0300

    task 2.1

---
+++ com.epamejc.lessons/src/homeworks/holidaysHW/arrays/randomCharArray.java
@@ -0,0 +1,30 @@
+package homeworks.holidaysHW.arrays;
+
+import java.util.Random;
+
+public class randomCharArray {
+
+    public static char[][] randomizeCharArray(int x, int y) {
+        char[][] charArray = new char[x][y];
+        Random rand = new Random();
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                charArray[i][j] = (char) (rand.nextInt(26) + 'a');
+            }
+        }
+        return charArray;
+    }
+
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 2;
+        char[][] charArray = randomizeCharArray(x, y);
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print(charArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
