11cc5ecd0a4e152ef01464ed321e4c790fa101cf
Neirwin <nightablackstone@gmail.com>
2020-01-25 08:16:08 +0300

    task 2.3

---
+++ com.epamejc.lessons/src/homeworks/holidaysHW/arrays/pyramid.java
@@ -0,0 +1,26 @@
+package homeworks.holidaysHW.arrays;
+
+import java.util.Scanner;
+
+public class pyramid {
+
+    public static void buildPyramid(int size) {
+        char[] pyrArray = new char[size];
+        for (int i = 0; i < size; i++) {
+            pyrArray[i] = 'X';
+        }
+        for (int i = size; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(pyrArray[j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter pyramid height: ");
+        int height = in.nextInt();
+        buildPyramid(height);
+    }
+}
