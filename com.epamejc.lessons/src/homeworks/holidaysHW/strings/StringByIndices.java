e8bad7eed92664cc40df00faae26cc4d3894f68b
Neirwin <nightablackstone@gmail.com>
2020-01-25 10:28:11 +0300

    Task 3.2

---
+++ com.epamejc.lessons/src/homeworks/holidaysHW/strings/StringByIndices.java
@@ -0,0 +1,39 @@
+package homeworks.holidaysHW.strings;
+
+import java.util.Scanner;
+
+public class StringByIndices {
+
+    public static void makeString(String inputString) {
+        Scanner in = new Scanner(System.in);
+        Integer index;
+        String resultString = "";
+
+        while (true) {
+            System.out.print("Enter index (or type 'exit' to finish): ");
+
+            if (in.hasNextInt()) {
+                index = in.nextInt();
+                while (index > inputString.length()) {
+                    System.out.println("Input index is bigger than string's length, enter again.");
+                    index = in.nextInt();
+                }
+                resultString += inputString.charAt(index);
+                System.out.println(resultString);
+            }
+
+            if (in.hasNextLine() && in.nextLine().equalsIgnoreCase("exit")) {
+                System.out.println(resultString);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input string: ");
+        String inString = in.nextLine();
+        makeString(inString);
+    }
+
+}
