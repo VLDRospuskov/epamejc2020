90d2d0557405bc7883da2e64a1eb12b83faa4b16
Neirwin <nightablackstone@gmail.com>
2020-01-25 11:39:32 +0300

    Task 4.1

---
+++ com.epamejc.lessons/src/homeworks/holidaysHW/recursion/Fibonacci.java
@@ -0,0 +1,17 @@
+package homeworks.holidaysHW.recursion;
+
+public class Fibonacci {
+
+    public static Integer fibonacciSeries(Integer numberPlace) {
+        if (numberPlace == 0 || numberPlace == 1) {
+            return 1;
+        }
+        return fibonacciSeries(numberPlace - 1) + fibonacciSeries(numberPlace - 2);
+    }
+
+    public static void main(String[] args) {
+        Integer n = fibonacciSeries(5);
+        System.out.print(n);
+    }
+
+}
