bb877aa7bcea5b5b534594a6b88903b055864bf6
NikolaevArtem <JqXRND7XUVgDJqc>
2020-01-28 01:30:18 +0300

    RandomChars with a/b strategy is done.

--- com.epamejc.lessons/src/homeworks/newYearHolidays/arrays/randomChars/RandomChars.java
+++ com.epamejc.lessons/src/homeworks/newYearHolidays/arrays/randomChars/RandomChars.java
@@ -7,18 +7,20 @@ import java.util.Arrays;
 public class RandomChars {
 
     public void run() {
-        Helper.showGreetingMessage();
         int amount = Helper.parseInt(Helper.getUserInput("Введите количество массивов: "));
         int length = Helper.parseInt(Helper.getUserInput("Введите длину массивов: "));
 
-        char [][] charArrays = fillWithRandomChars(new char[amount][length]);
-
+        char[][] charArrays = fillWithRandomChars(new char[amount][length]);
         printCharArrays(charArrays);
+
+        String strategy = Helper.getUserInput("Введите стратегию: ");
+        printFormattedString(charArrays, strategy);
+        Helper.closeReader();
     }
 
-    private char [][] fillWithRandomChars(char[][] charArrays) {
-        for (char [] chars: charArrays) {
-            for (int i = 0; i < chars.length; i++){
+    private char[][] fillWithRandomChars(char[][] charArrays) {
+        for (char[] chars : charArrays) {
+            for (int i = 0; i < chars.length; i++) {
                 chars[i] = getRandomChar();
             }
         }
@@ -30,9 +32,43 @@ public class RandomChars {
         return (char) ('a' + randomNumberFrom26);
     }
 
-    private void printCharArrays(char [][] charArrays) {
-        for (char [] chars: charArrays) {
+    private void printCharArrays(char[][] charArrays) {
+        for (char[] chars : charArrays) {
             System.out.println(Arrays.toString(chars));
         }
+    }
+
+    private void printFormattedString(char[][] charArrays, String strategy) {
+        String evenChars = "";
+        String oddChars = "";
+
+        for (char[] chars : charArrays) {
+            for (int i = 0; i < chars.length; i++) {
+                if (i % 2 == 0) {
+                    oddChars += chars[i];
+                } else {
+                    evenChars += chars[i];
+                }
+            }
+        }
+
+        String result = getEvenOrOddChars(evenChars, oddChars, strategy);
+        System.out.println(result);
+    }
+
+    private String getEvenOrOddChars(String evenChars, String oddChars, String strategy) {
+        String result;
+
+        if (strategy.equalsIgnoreCase("a")) {
+            result = oddChars;
+        } else if (strategy.equalsIgnoreCase("b")) {
+            result = evenChars;
+        } else {
+            System.out.println("Стратегия выбрана неверно!");
+            String strategy2 = Helper.getUserInput("Введите стратегию: ");
+            result = getEvenOrOddChars(evenChars, oddChars, strategy2);
+        }
+
+        return result;
     }
 }
