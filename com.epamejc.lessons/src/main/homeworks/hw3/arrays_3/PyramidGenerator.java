62c1c92078877a9f92a78eccc59c81be8bdf5f32
Vitaly Bogdanoff <nightstranger@mail.ru>
2020-01-27 15:49:01 +0300

    Refactor PyramidGenerator HW3 - 3

--- com.epamejc.lessons/src/main/homeworks/hw3/arrays_3/PyramidGenerator.java
+++ com.epamejc.lessons/src/main/homeworks/hw3/arrays_3/PyramidGenerator.java
@@ -1,22 +1,30 @@
-package main.homeworks.hw2.arrays_2_3;
+package main.homeworks.hw3.arrays_3;
 
 import java.util.Scanner;
 
-public class PyramidGen {
-    public static void main(String[] args) {
+public class PyramidGenerator {
+
+    public void run() {
+        printPyramid(scanNumber());
+    }
+
+    public int scanNumber() {
         Scanner scan = new Scanner(System.in);
         int n = 0;
         int nCopy = 0;
         try {
             System.out.print("Enter number: ");
             n = scan.nextInt();
-            nCopy = n;
         } catch (Exception e) {
             System.out.println("Error! " + e);
         } finally {
             scan.close();
         }
+        return n;
+    }
 
+    public void printPyramid(int n) {
+        int nCopy = n;
         char[][] arr = new char[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < nCopy; j++) {
@@ -27,4 +35,5 @@ public class PyramidGen {
             System.out.println("");
         }
     }
+
 }
