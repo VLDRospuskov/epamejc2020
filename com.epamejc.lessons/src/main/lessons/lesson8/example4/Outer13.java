f18fc3be09bd8f36a12e4524d0c0248e7b9cbbbb
Vladislav Rospuskov <Vladislav_Rospuskov@epam.com>
2020-01-29 20:25:52 +0300

    Add lesson8

---
+++ com.epamejc.lessons/src/main/lessons/lesson8/example4/Outer13.java
@@ -0,0 +1,22 @@
+package main.lessons.lesson8.example4;
+
+public class Outer13 {
+
+    static int x = 10;
+
+    static class Inner1 {
+        public static void method() {
+            System.out.println("inner1 outer.x=" + x);
+        }
+    }
+
+}
+
+class Outer14 extends Outer13.Inner1 {
+
+    public void outer2Method() {
+//         System.out.println("x="+ x); // ERROR
+    }
+
+}
+
