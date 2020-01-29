6bcd1d48d74805fa6f1172c27eef940cf4e4bd29
NikolaevArtem <JqXRND7XUVgDJqc>
2020-01-29 10:15:29 +0300

    Updated and simplified TrafficLight

--- com.epamejc.lessons/src/homeworks/lesson1/trafficLight/TrafficLight.java
+++ com.epamejc.lessons/src/homeworks/lesson1/trafficLight/TrafficLight.java
@@ -1,69 +1,40 @@
 package homeworks.lesson1.trafficLight;
 
+import homeworks.utility.helper.Helper;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 class TrafficLight {
-    private final String EXIT_WORD = "exit";
-    private final String GREET_MESSAGE = "Для выхода введите \"" + EXIT_WORD + "\"\n";
+
+    private final String GREET_MESSAGE = "Для выхода введите \"exit\"";
     private final String FINAL_MESSAGE = "Спасибо за пользование программой!";
 
     void run() {
         System.out.println(GREET_MESSAGE);
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            boolean isRunning = true;
-
-            while (isRunning) {
-                int n = getIntFromUser(reader);
-
-                if (n == -1) {
-                    isRunning = false;
-                    continue;
-                } else if (n == -2) {
-                    continue;
-                }
-
-                checkTrafficLight(n);
-            }
-        } catch (IOException e){
-            System.out.println("Что-то пошло не так c вводом/выводом!");
-        }
-    }
-
-    private int getIntFromUser(BufferedReader reader) {
-        try {
-            System.out.print("Введите количество минут с начала работы светофора: ");
-            String s = reader.readLine();
-
-            if (s.equalsIgnoreCase(EXIT_WORD)) {
+        boolean isRunning = true;
+        while (isRunning) {
+            String s = Helper.getUserInput("Введите количество секунд с начала работы светофора: ");
+            if (s.equals("exit")) {
+                isRunning = false;
                 System.out.println(FINAL_MESSAGE);
-                return -1;
+                continue;
             }
 
-            int n = Integer.parseInt(s);
-
-            if (n < 0) {
-                System.out.println("Введите число больше нуля!");
-                return -2;
+            int n = Helper.parseInt(s);
+            if (n > 0) {
+                printColorTrafficLight(n);
             } else {
-                return n;
+                System.out.println("Введите положительное значение!");
             }
 
-        } catch (NumberFormatException e) {
-            System.out.println("Попробуйте еще раз. Возможно, вместо цифр вы написали буквы, или число " +
-                    "слишком длинное");
-            return -2;
-
-        } catch (Exception e) {
-            System.out.println("Что-то пошло не так с вводимым значением!");
-            e.printStackTrace();
-            return -1;
+            System.out.println("---------------------------------------");
         }
     }
 
-    private void checkTrafficLight(int n) {
+    private void printColorTrafficLight(int n) {
         n = n % 10;
 
         if (0 <= n && n <= 3) {
