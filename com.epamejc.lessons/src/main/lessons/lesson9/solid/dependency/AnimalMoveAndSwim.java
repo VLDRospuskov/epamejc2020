7ef9064d389919bc03f7342000c9f293396a4f3f
Vladislav Rospuskov <Vladislav_Rospuskov@epam.com>
2020-01-31 19:32:43 +0300

    Add lesson9

---
+++ com.epamejc.lessons/src/main/lessons/lesson9/solid/dependency/AnimalMoveAndSwim.java
@@ -0,0 +1,11 @@
+package main.lessons.lesson9.solid.dependency;
+
+public interface AnimalMoveAndSwim {
+
+    void swim();
+
+    default void walk() {
+        System.out.println("Cant walk");
+    }
+
+}
---
+++ com.epamejc.lessons/src/main/lessons/lesson9/solid/single/Animal.java
@@ -0,0 +1,22 @@
+package main.lessons.lesson9.solid.single;
+
+import lombok.ToString;
+
+@ToString
+public class Animal {
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
