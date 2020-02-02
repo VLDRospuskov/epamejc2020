94579da343a28b6c1de562049bdc60c115f6d94c
shandakova <shandakova98@mail.ru>
2020-02-03 00:31:45 +0300

    Add annotation homework

---
+++ com.epamejc.lessons/src/homework/homework6/annotation/Main.java
@@ -0,0 +1,8 @@
+package homework.homework6.annotation;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person);
+    }
+}
