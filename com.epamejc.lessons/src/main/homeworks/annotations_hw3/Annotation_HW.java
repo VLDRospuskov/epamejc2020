d9da314c33867d0bacd1059e45e57e1f068c89d2
Kirill Turchinskii <turchinskii.kirill@gmail.com>
2020-02-05 17:22:21 +0300

    Annotation task is done

---
+++ com.epamejc.lessons/src/main/homeworks/annotations_hw3/Annotation_HW.java
@@ -0,0 +1,10 @@
+package main.homeworks.annotations_hw3;
+
+public class Annotation_HW {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person);
+
+    }
+}
