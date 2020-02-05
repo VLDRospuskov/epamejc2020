8976957618bbd64222ade2c57cd83bacbbdcd301
koshreality <koshreality@gmail.com>
2020-02-05 14:51:28 +0300

    hw annotations

---
+++ com.epamejc.lessons/src/main/homework/Annotations/Main.java
@@ -0,0 +1,12 @@
+package homework.Annotations;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Annotated annotated = new Annotated();
+            System.out.println(annotated);
+        } catch (NoSuchMethodException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
