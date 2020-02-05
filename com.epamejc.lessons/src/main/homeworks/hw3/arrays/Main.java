5e3b21cd2618ac220e634a97cb0d6487ee5c4f58
karinaria <58939360+karinaria@users.noreply.github.com>
2020-02-05 15:51:07 +0300

    HW1, 2, 3, 4, 5 refactor

--- com.epamejc.lessons/src/main/homeworks/hw3/arrays/Main.java
+++ com.epamejc.lessons/src/main/homeworks/hw3/arrays/Main.java
@@ -1,4 +1,11 @@
 package main.homeworks.hw3.arrays;
 
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Logic runner = new Logic();
+        runner.run();
+    }
 }
