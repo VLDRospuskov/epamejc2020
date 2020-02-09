7fdcb9184fc486d38a017453c7b6ea4767e3283b
Vitaly Bogdanoff <nightstranger@mail.ru>
2020-02-09 20:03:22 +0300

    Add default values to annotation.

--- com.epamejc.lessons/src/main/homeworks/java/hw6/PersonFields.java
+++ com.epamejc.lessons/src/main/homeworks/java/hw6/PersonFields.java
@@ -9,8 +9,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PersonFields {
 
-    String name();
-    int age();
+    String name() default "Name";
+    int age() default 0;
 
 
 }
