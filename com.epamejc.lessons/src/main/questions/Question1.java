ead7586d026434e1745d334c413c3f4340fa219c
Vladimir Ivanov <spellwhisper@mail.ru>
2020-02-12 02:40:10 +0300

    question added

---
+++ com.epamejc.lessons/src/main/questions/Question1.java
@@ -0,0 +1,15 @@
+package questions;
+
+public class Question1 {
+
+    public String method (int i, Object o) {
+        return methodPrivate("something", i, o);
+    }
+
+    private String methodPrivate(String something, int i, Object o) {
+        /**
+         * some logic here
+         */
+        return "stuff";
+    }
+}
