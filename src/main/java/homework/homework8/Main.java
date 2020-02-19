d00aabb3f18411d6de6399945a9f0c84592b1522
Lenovo <olegfom97@mail.ru>
2020-02-19 15:27:58 +0300

    homework8 added

---
+++ src/main/java/homework/homework8/Main.java
@@ -0,0 +1,14 @@
+package homework.homework8;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Buffered buffered = new Buffered();
+        NonBuffered nonBuffered = new NonBuffered();
+        BufferedLine bufferedLine = new BufferedLine();
+
+        System.out.printf("BufferedLine time: %d nanoseconds\nBuffered time:     %d nanoseconds\nNonBuffered time:  %d nanoseconds",
+                           bufferedLine.measureTime(), buffered.measureTime(), nonBuffered.measureTime());
+    }
+}
