7a6f0bca786e341c81dbfd1c043660a26527b49c
Alex F <desdichago@gmail.com>
2020-02-20 19:31:08 +0300

    Created Annotation

--- com.epamejc.lessons/src/main/java/Homeworks/HW4/Main.java
+++ com.epamejc.lessons/src/main/java/Homeworks/HW4/Main.java
@@ -2,17 +2,8 @@ package Homeworks.HW4;
 
 
 public class Main {
-
-    @MyAnnotation(name="Alice", age=30)
-    static Person person;
-
     public static void main(String[] args) {
-        person =  new Person();
-        //person1.toString(); // error
-
-      //  @MyAnnotation(name="Bob", age=20)
-    //    Person person2 = new Person();
-        String name = person.getClass().getDeclaredAnnotation(MyAnnotation.class).name();
-        System.out.println(name);
+        Person person =  new Person();
+        System.out.println(person.toString());
     }
 }
--- com.epamejc.lessons/src/main/java/Homeworks/HW4/Person.java
+++ com.epamejc.lessons/src/main/java/Homeworks/HW4/Person.java
@@ -1,13 +1,24 @@
 package Homeworks.HW4;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+
 public class Person {
 
     private String name;
     private int age;
 
 
+    @MyAnnotation(name="Alice", age=30)
     public Person() {
         // empty constructor
+
+        // reflection
+        MyAnnotation myAnnotation = getClass().getDeclaredConstructors()[0].getAnnotation(MyAnnotation.class);
+        this.name = myAnnotation.name();
+        this.age = myAnnotation.age();
     }
 
     @Override
