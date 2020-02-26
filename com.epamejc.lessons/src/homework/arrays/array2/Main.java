b985c2a2170f1daf7e929200a29511bd4d600ba7
DarPru <darya.prusowa@gmail.ru>
2020-02-26 11:43:41 +0300

    array homework change

---
+++ com.epamejc.lessons/src/homework/arrays/array2/Main.java
@@ -0,0 +1,8 @@
+package homework.arrays.array2;
+
+public class Main {
+    public static void main(String[] args) {
+        arrayStrategy arrayStrategy = new arrayStrategy();
+        arrayStrategy.run();
+    }
+}
