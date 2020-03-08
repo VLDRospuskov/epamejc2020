7d2660cc3ea52c4cb48de24bd98c3d8c5bc7fd3d
DarPru <darya.prusowa@gmail.ru>
2020-03-08 20:14:47 +0300

    string homework change

---
+++ com.epamejc.lessons/src/homework/recursion/recursion3/Main.java
@@ -0,0 +1,9 @@
+package homework.recursion.recursion3;
+
+public class Main {
+    public static void main(String[] args) {
+        DigitAmount digitAmount = new DigitAmount();
+        digitAmount.run();
+
+    }
+}
