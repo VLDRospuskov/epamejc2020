416f1f6c989b38eec6477dc9c8525fae31564317
Kirill Turchinskii <turchinskii.kirill@gmail.com>
2020-03-09 05:59:46 +0300

    FileReader and BufferedReader comparison is done

---
+++ com.epamejc.lessons/src/main/java/homeworks/homework4/ReaderHomework.java
@@ -0,0 +1,10 @@
+package homeworks.homework4;
+
+public class ReaderHomework {
+
+    public static void main(String[] args) {
+        ReaderComparison readerComparison = new ReaderComparison();
+        readerComparison.compareTime();
+    }
+
+}
