53973f1553c2cbce071afdbfbea314deb00e3678
Karpovich Art <karpovichart@gmail.com>
2020-03-10 17:35:39 +0300

    SB Start

--- com.epamejc.lessons/src/main/java/seabattle/Main.java
+++ com.epamejc.lessons/src/main/java/seabattle/Main.java
@@ -1,7 +1,40 @@
 package seabattle;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+    void parsCommand(){}//todo Сделать метод который будет парсть команды
+}
+
+class Pole {
+    private List<List<Cell>> Ships;
+
+
+}
+
+enum ships {
+    SHIP1,
+    SHIP2,
+    SHIP3,
+    SHIP4
+}
+
+enum state {
+    SHIP,
+    HIT,
+    CLEAR
+}
+
+class Cell {
+    private ships typeOfShip;
+    private state state;
+
+}
+
+class user{
+    private List<String> ships;//todo Сделать такую структуру, что бы можно было хранить количество и тип кораблей для поатвновки на доску
+
 }
